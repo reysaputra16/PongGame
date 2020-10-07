e03b5bd20ae1ce0e485618361e589d3e33f0412b
Reynard Saputra <58236360+reysaputra16@users.noreply.github.com>
2020-10-07 13:07:34 +0200

    JavaFX app created

---
+++ src/main/java/com/javafx/ponggame/App.java
@@ -0,0 +1,17 @@
+package com.javafx.ponggame;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Pong Game");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
