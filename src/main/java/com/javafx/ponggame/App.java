4e0f7c65d3e16fe8bb292b0b34ba712c0ec758f8
Reynard Saputra <58236360+reysaputra16@users.noreply.github.com>
2020-10-12 21:12:32 +0200

    Player is on gameboard

--- src/main/java/com/javafx/ponggame/App.java
+++ src/main/java/com/javafx/ponggame/App.java
@@ -1,14 +1,29 @@
 package com.javafx.ponggame;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
+    private final PlayerStick leftPlayer = new PlayerStick(0, 325);
+    private final PlayerStick rightPlayer = new PlayerStick(1260, 325);
+
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
+
+        Pane root = new Pane();
+        Scene scene = new Scene(root);
+        root.getChildren().add(leftPlayer);
+        root.getChildren().add(rightPlayer);
+
+        primaryStage.setScene(scene);
         primaryStage.setTitle("Pong Game");
         primaryStage.show();
+        primaryStage.setFullScreen(true);
+
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/javafx/ponggame/PlayerStick.java
@@ -0,0 +1,19 @@
+package com.javafx.ponggame;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class PlayerStick extends Rectangle {
+
+    private int speed = 0;
+
+    public PlayerStick(double x, double y) {
+        super(20, 100, Color.BLACK);
+        this.setX(x);
+        this.setY(y);
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
