4bebe52a35b09b85c9e33b051625270618546c9e
Reynard Saputra <58236360+reysaputra16@users.noreply.github.com>
2020-10-13 11:11:46 +0200

    Right player movement cannot leave the screen

--- src/main/java/com/javafx/ponggame/App.java
+++ src/main/java/com/javafx/ponggame/App.java
@@ -21,9 +21,10 @@ public class App extends Application {
         root.getChildren().add(rightPlayer);
 
         scene.setOnKeyPressed(e -> {
-            if(e.getCode() == KeyCode.UP) {
+            if(e.getCode() == KeyCode.UP && rightPlayer.getY() > 0) {
                 rightPlayer.setY(rightPlayer.getY() - 5);
-            } else if(e.getCode() == KeyCode.DOWN) {
+            } else if(e.getCode() == KeyCode.DOWN &&
+                    rightPlayer.getY() < scene.getHeight() - rightPlayer.getHeight()) {
                 rightPlayer.setY(rightPlayer.getY() + 5);
             }
         });
